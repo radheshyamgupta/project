2d8091d81ec034048a447c68c4702819d2abc844
Radheshyam gupta <130499661+radheshyamgupta@users.noreply.github.com>
2023-05-25 10:18:00 +0530

    deletFunctionalityAdded

--- expense tracker1.0/EepenseTracker2/test.js
+++ expense tracker1.0/EepenseTracker2/test.js
@@ -4,31 +4,52 @@ var description = document.querySelector("#description");
 var category = document.querySelector("#category");
 var form = document.querySelector("#expense-form");
 var expenseList = document.querySelector("#expense-list");
-
-function populateExpenseList(response) {
+function populateUserList(response) {
   expenseList.innerHTML = "";
 
   if (response.length > 0) {
-    response.forEach(function (expense, index) {
+    response.forEach(function (user, index) {
       var li = document.createElement("li");
       li.innerHTML = `
-        <span>₹${expense.amount}</span>
-        <span>${expense.description}</span>
-        <span>${expense.category}</span>
-        <button class="delete-btn" data-index="${index}">Delete</button>
-        <button class="edit-btn" data-index="${index}">Edit</button>
+        <span>${user.amount}</span>
+        <span>${user.description}</span>
+        <span>${user.category}</span>
+        <button class="delete-btn" data-id="${user._id}">&#10006;</button>
       `;
       expenseList.appendChild(li);
     });
+
+ 
+    var deleteButtons = document.querySelectorAll(".delete-btn");
+    deleteButtons.forEach(function (button) {
+      button.addEventListener("click", deleteUser);
+    });
   }
 }
+
+function deleteUser(e) {
+  var userId = e.target.dataset.id;
+
+  axios.delete(`https://crudcrud.com/api/98728a5fa17c4ccaa3f3a4c425efede3/endpoint/${userId}`)
+    .then((response) => {
+      console.log(response);
+      e.target.parentElement.remove(); // Removed the user detail from the website
+      displayData()
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+// ...
+
 
 window.addEventListener("DOMContentLoaded", displayData);
 
 function displayData() {
   axios.get('https://crudcrud.com/api/98728a5fa17c4ccaa3f3a4c425efede3/endpoint')
     .then((response) => {
-      populateExpenseList(response.data);
+      populateUserList(response.data);
       console.log(response.data);
     })
     .catch((err) => {
@@ -56,7 +77,7 @@ function addExp(e) {
       .then((response) => {
         console.log(response);
         form.reset();
-        populateExpenseList(response.data); // Update the expense list with the new expense
+        populateUserList(response.data); // Update the expense list with the new expense
         window.location.reload();
       })
       .catch((err) => {
