6ce47fb7fd0702e2a30385b341cf7641d8b2784a
Radheshyam gupta <130499661+radheshyamgupta@users.noreply.github.com>
2023-05-25 06:12:34 +0530

    axios.get functionality added successfully

--- expense tracker1.0/EepenseTracker2/test.js
+++ expense tracker1.0/EepenseTracker2/test.js
@@ -5,15 +5,11 @@ var category = document.querySelector("#category");
 var form = document.querySelector("#expense-form");
 var expenseList = document.querySelector("#expense-list");
 
-// Retrieve expenses from local storage or initialize an empty array
-var expenses = JSON.parse(localStorage.getItem("expenses")) || [];
-
-// Populate expense list with saved expenses
-function populateExpenseList() {
+function populateExpenseList(response) {
   expenseList.innerHTML = "";
 
-  if (expenses.length > 0) {
-    expenses.forEach(function (expense, index) {
+  if (response.length > 0) {
+    response.forEach(function (expense, index) {
       var li = document.createElement("li");
       li.innerHTML = `
         <span>₹${expense.amount}</span>
@@ -27,7 +23,18 @@ function populateExpenseList() {
   }
 }
 
-populateExpenseList();
+window.addEventListener("DOMContentLoaded", displayData);
+
+function displayData() {
+  axios.get('https://crudcrud.com/api/98728a5fa17c4ccaa3f3a4c425efede3/endpoint')
+    .then((response) => {
+      populateExpenseList(response.data);
+      console.log(response.data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
 
 form.addEventListener("submit", addExp);
 
@@ -45,68 +52,17 @@ function addExp(e) {
       category: categoryData,
     };
 
-    // Add new expense to the array
-    expenses.push(expense);
-    axios.post('https://crudcrud.com/api/98728a5fa17c4ccaa3f3a4c425efede3/endpoint',expense)
-    .then((response)=>{
-      console.log(response)
-    })
-    .catch((err)=>{
-console.log(err)
-    })
-
-    // Store the updated expenses array in local storage
-    localStorage.setItem("expenses", JSON.stringify(expenses));
-
-    // Clear form fields
-    form.reset();
-
-    // Update the expense list on the web page
-    populateExpenseList();
+    axios.post('https://crudcrud.com/api/98728a5fa17c4ccaa3f3a4c425efede3/endpoint', expense)
+      .then((response) => {
+        console.log(response);
+        form.reset();
+        populateExpenseList(response.data); // Update the expense list with the new expense
+        window.location.reload();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+      
   }
-}
-
-expenseList.addEventListener("click", handleExpenseListClick);
-
-function handleExpenseListClick(e) {
-  if (e.target.classList.contains("delete-btn")) {
-    var index = e.target.dataset.index;
-    deleteExpense(index);
-  } else if (e.target.classList.contains("edit-btn")) {
-    var index = e.target.dataset.index;
-    editExpense(index);
-  }
-}
-
-function deleteExpense(index) {
-  if (index >= 0 && index < expenses.length) {
-    // Remove the expense at the specified index
-    expenses.splice(index, 1);
-
-    // Store the updated expenses array in local storage
-    localStorage.setItem("expenses", JSON.stringify(expenses));
-
-    // Update the expense list on the web page
-    populateExpenseList();
-  }
-}
-
-function editExpense(index) {
-  if (index >= 0 && index < expenses.length) {
-    var expense = expenses[index];
-
-    // Populate the form fields with the expense data
-    amount.value = expense.amount;
-    description.value = expense.description;
-    category.value = expense.category;
-
-    // Remove the expense from the array
-    expenses.splice(index, 1);
-
-    // Store the updated expenses array in local storage
-    localStorage.setItem("expenses", JSON.stringify(expenses));
-
-    // Update the expense list on the web page
-    populateExpenseList();
-  }
+  
 }
